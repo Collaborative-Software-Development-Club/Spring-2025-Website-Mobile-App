b10c9515ff0b2b1cbbad7e5dd84366874da3c371
danielgof <gofmandon@gmail.com>
2025-04-19 15:47:34 -0400

    added actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://spring-2025-website-mobile-app.github.io',
+  base: 'https://github.com/Collaborative-Software-Development-Club/Spring-2025-Website-Mobile-App#',
+});
